d21d8d4e073439a6bac7c6feb02cb597286c5a70
gourav <gouravsharma99285@gmail.com>
2022-09-20 09:46:33 +0530

    deploy under process

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState, useEffect } from 'react'
-import Logo from './Logo'
+// import Logo from './Logo'
 import Loader from 'react-loaders'
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -57,7 +57,7 @@ const Home = () => {
             CONTACT ME
           </Link>
         </div>
-        <Logo />
+        {/* <Logo /> */}
       </div>
       <Loader type="pacman" />
     </>
